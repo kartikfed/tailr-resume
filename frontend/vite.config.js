219de0c498f45b5d2d6f54a8e422c9cecca0a226
Kartik Krishnan <kartikkrishnan6@gmail.com>
2025-05-17 11:10:22 -0400

    vercel deployment changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,5 +6,13 @@ export default defineConfig({
   server: {
     port: 5173,
     open: true
+  },
+  build: {
+    rollupOptions: {
+      external: ['remark-gfm'],
+    },
+  },
+  optimizeDeps: {
+    include: ['remark-gfm']
   }
 });
